74bfaf4c8a782cb7b03bb7502607bbf1e83d1685
Rupert Key <Rupert.Key@GMail.com>
2016-01-02 22:45:08 +0000

    PredictiveAim.cs updated to Kain's 2016/01/02 version... New version used but requires 'faked' test use 'Rupert' algorithm implementation to show viability. Fake test case improved so runs without requiring manual invocation. README.md updated with more explanation.

--- Assets/01UGS/02Code/UnityUtils/GamePlayUtils/PredictiveAim.cs
+++ Assets/01UGS/02Code/UnityUtils/GamePlayUtils/PredictiveAim.cs
@@ -36,6 +36,16 @@ using UnityEngine;
 
 class GameUtilities
 {
+	//////////////////////////////////////////////////////////////////////////////
+	//This implies that no solution exists for this situation as the target may literally outrun the projectile with its current direction
+	//In cases like that, we simply aim at the place where the target will be 1 to 5 seconds from now.
+	//Feel free to randomize t at your discretion for your specific game situation if you want that guess to feel appropriately noisier
+	static float PredictiveAimWildGuessAtImpactTime()
+	{
+		return Random.Range(1, 5);
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
 	//Return a non-normalized vector representing the initial velocity
 	//of a lobbed projectile in 3D space
 	//meant to hit a target moving at constant velocity
@@ -44,47 +54,97 @@ class GameUtilities
 	//gravity is assumed to be a positive number. It will be calculated in the downward direction
 	static public Vector3 PredictiveAim(Vector3 muzzlePosition, float projectileSpeed, Vector3 targetPosition, Vector3 targetVelocity, float gravity)
 	{
+		Debug.Assert(projectileSpeed > 0, "What are you doing shooting at something with a projectile that doesn't move?");
 		if (muzzlePosition == targetPosition)
 		{
 			//Why dost thou hate thyself so?
-			return Vector3.zero;
+			//Do something smart here. I dunno... whatever.
+			return projectileSpeed * (Random.rotation * Vector3.forward);
 		}
 
 		//Much of this is geared towards reducing floating point precision errors
 		float projectileSpeedSq = projectileSpeed * projectileSpeed;
-
 		float targetSpeedSq = targetVelocity.sqrMagnitude; //doing this instead of self-multiply for maximum accuracy
 		float targetSpeed = Mathf.Sqrt(targetSpeedSq);
-
 		Vector3 targetToMuzzle = muzzlePosition - targetPosition;
 		float targetToMuzzleDistSq = targetToMuzzle.sqrMagnitude; //doing this instead of self-multiply for maximum accuracy
 		float targetToMuzzleDist = Mathf.Sqrt(targetToMuzzleDistSq);
-
 		Vector3 targetToMuzzleDir = targetToMuzzle;
 		targetToMuzzleDir.Normalize();
 
 		Vector3 targetVelocityDir = targetVelocity;
 		targetVelocityDir.Normalize();
 
-		//Law of Cosines: a*a+ b*b - 2*a*b*cos(theta) = c*c
-		//Quadratic formula: t = [ -b ± Sqrt( b*b - 4*a*c ) ] / (2*a)
+		//Law of Cosines: A*A + B*B - 2*A*B*cos(theta) = C*C
+		//A is distance from muzzle to target (known value: targetToMuzzleDist)
+		//B is distance traveled by target until impact (targetSpeed * t)
+		//C is distance traveled by projectile until impact (projectileSpeed * t)
 		float cosTheta = Vector3.Dot(targetToMuzzleDir, targetVelocityDir);
-		float a = projectileSpeedSq - targetSpeedSq;
-		float b = 2.0f * targetToMuzzleDist * targetSpeed * cosTheta;
-		float c = -targetToMuzzleDistSq;
 
-		float uglyNumber = Mathf.Sqrt(b * b - 4.0f * a * c);
-		float t0 = 0.5f * (-b + uglyNumber) / a;
-		float t1 = 0.5f * (-b - uglyNumber) / a;
-		float t = Mathf.Infinity;
-		//Assign the lowest positive time to t
-		t = Mathf.Min(t0, t1);
-		if (t < 0)
+		float t;
+		if (Mathf.Approximately(projectileSpeedSq, targetSpeedSq))
 		{
-			t = Mathf.Max(t0, t1);
+			//a = projectileSpeedSq - targetSpeedSq = 0
+			//We want to avoid div/0 that can result from target and projectile traveling at the same speed
+			//We know that C and B are the same length because the target and projectile will travel the same distance to impact
+			//Law of Cosines: A*A + B*B - 2*A*B*cos(theta) = C*C
+			//Law of Cosines: A*A + B*B - 2*A*B*cos(theta) = B*B
+			//Law of Cosines: A*A - 2*A*B*cos(theta) = 0
+			//Law of Cosines: A*A = 2*A*B*cos(theta)
+			//Law of Cosines: A = 2*B*cos(theta)
+			//Law of Cosines: A/(2*cos(theta)) = B
+			//Law of Cosines: 0.5f*A/cos(theta) = B
+			//Law of Cosines: 0.5f * targetToMuzzleDist / cos(theta) = targetSpeed * t
+			//We know that cos(theta) of zero or less means there is no solution, since that would mean B goes backwards or leads to div/0 (infinity)
+			if (cosTheta > 0)
+			{
+				t = 0.5f * targetToMuzzleDist / (targetSpeed * cosTheta);
+			}
+			else
+			{
+				t = PredictiveAimWildGuessAtImpactTime();
+			}
+		}
+		else
+		{
+			//Quadratic formula: Note that lower case 'a' is a completely different derived variable from capital 'A' used in Law of Cosines (sorry):
+			//t = [ -b ï¿½ Sqrt( b*b - 4*a*c ) ] / (2*a)
+			float a = projectileSpeedSq - targetSpeedSq;
+			float b = 2.0f * targetToMuzzleDist * targetSpeed * cosTheta;
+			float c = -targetToMuzzleDistSq;
+			float discriminant = b * b - 4.0f * a * c;
+
+			if (discriminant < 0)
+			{
+				//Square root of a negative number is an imaginary number (NaN)
+				//Special thanks to Rupert Key (Twitter: @Arakade) for exposing NaN values that occur when target speed is faster than or equal to projectile speed
+				t = PredictiveAimWildGuessAtImpactTime();
+			}
+			else
+			{
+				//a will never be zero because we protect against that with "if (Mathf.Approximately(projectileSpeedSq, targetSpeedSq))" above
+				float uglyNumber = Mathf.Sqrt(discriminant);
+				float t0 = 0.5f * (-b + uglyNumber) / a;
+				float t1 = 0.5f * (-b - uglyNumber) / a;
+				//Assign the lowest positive time to t to aim at the earliest hit
+				t = Mathf.Min(t0, t1);
+				if (t < Mathf.Epsilon)
+				{
+					t = Mathf.Max(t0, t1);
+				}
+
+				if (t < Mathf.Epsilon)
+				{
+					//Time can't flow backwards when it comes to aiming.
+					//No real solution was found, take a wild shot at the target's future location
+					t = PredictiveAimWildGuessAtImpactTime();
+				}
+			}
 		}
 
 		//Vb = Vt - 0.5*Ab*t + [(Pti - Pbi) / t]
+		//By adding gravity as projectile acceleration, we are essentially breaking real world rules by saying that the projectile
+		// gets any upwards/downwards gravity compensation velocity for free, since the projectileSpeed passed in is a constant that assumes zero gravity
 		Vector3 projectileAcceleration = gravity * Vector3.down;
 		Vector3 Vb = targetVelocity - (0.5f * projectileAcceleration * t) + (-targetToMuzzle / t);
 		//FOR CHECKING ONLY (valid only if gravity is 0)...
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ From an intuitive point of view, both of these cases *can* have valid shooting s
  - Open scene "01UGS/01Scenes/PreemptiveAimingTest01.unity".
  - Open Menu "Unity Test Tools | Integration Test Runner".
  - On new window, press "Run All".
+ - Note failure of both cases where target velocity exceeds bullet velocity.
 
 ## Current expectation
 
@@ -25,9 +26,12 @@ There are 2 groups of tests that will pass and 1 group that currently fail:
 
  - Fail:
   - 03 Fast Moving tests
-    *(although "03 Moving + ShotNoGrav + aimFaked" will pass and be ignored -- it is there to show a valid solution exists despite algorithm being unable to find it.)*
+
+    *(although "03 Moving + ShotNoGrav + aimFaked" will pass, it is a fake!  It is a duplicate of "01 Moving + ShotNoGrav" but with the Shooter turns 30 degrees away from travel.  It is there to show a valid solution exists despite algorithm being unable to find it.  
+    Update 2016/01/02 now uses 'Rupert' implementation of the algorithm since Kain's now attempts to kludge aim.)*
 
 ----
 
 N.b. If you're not familiar with "Unity Test Tools", you can change whether interaction is allowed during tests and toggle "pause on failures" from the icon in the top-right corner of the "Integration Tests" window (down-arrow + 3 horizontal lines).
+
 
